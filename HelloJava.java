7aab477e850b91ea7318d44700c50d84452987b4
Ubuntu <ganesh.mv1@gmail.com>
2020-04-02 08:29:43 +0000

    pushing all the files to github

---
+++ HelloJava.java
@@ -0,0 +1,5 @@
+class HelloJava{
+public static void main(String[] args){
+System.out.Println("THis is a java program by Ganesh.");
+}
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+var texts = "This is a javascript file by Ganesh.";
+console.log(texts);
